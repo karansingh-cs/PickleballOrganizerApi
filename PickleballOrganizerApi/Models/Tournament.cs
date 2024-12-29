1db7b355d4b52c9fe2a1291c6e19fa05351dcc6a
karansingh-cs <karanbir97@outlook.com>
2024-12-28 20:48:45 -0600

    Added Tournament class inside Models folder to match the Database columns

---
+++ PickleballOrganizerApi/Models/Tournament.cs
@@ -0,0 +1,12 @@
+﻿namespace PickleballOrganizerApi.Models
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string FirstPlayer { get; set; }  = string.Empty;
+        public string SecondPlayer { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+    }
+}
