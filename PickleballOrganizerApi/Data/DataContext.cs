1e58754b8d988f172737c5cafb167c1f5a368a4a
karansingh-cs <karanbir97@outlook.com>
2024-12-29 20:12:36 -0600

    Added the DataContext class and Services in the program.cs

---
+++ PickleballOrganizerApi/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PickleballOrganizerApi.Models;
+
+namespace PickleballOrganizerApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Tournament> Tournaments { get; set; }
+    }
+}
